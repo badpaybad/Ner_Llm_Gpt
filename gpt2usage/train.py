ae1cafd0d7d97d42f7ca97aeda8c4cf0ba994f58
dunp <badpaybad@gmail.com>
2024-04-16 10:15:26 +0700

    test code runable with custom dataset

--- gpt2usage/train.py
+++ gpt2usage/train.py
@@ -5,4 +5,11 @@ gpt2_model, gpt2_tokenizer= gpt2helper.build_pretrain_model_tokenizer()
 
 joke_loader = gpt2helper.build_joker_loader()
 
-gpt2helper.gpt2_predict_test(gpt2_model, gpt2_tokenizer,gpt2helper.MODEL_EPOCH)
+gpt2helper.gpt2_train( gpt2_model,gpt2_tokenizer,joke_loader=joke_loader)
+
+gpt2helper.gpt2_predict_test(gpt2_model, gpt2_tokenizer, model_epoch_name="gpt2_medium_joker_4.pt" ,
+                            models_folder="/work/Ner_Llm_Gpt/gpt2usage/gpt2_trained_models")
+
+
+# after train and got model into folder gpt2_trained_models 
+# run test.py to generate from input text
