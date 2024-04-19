1b0e948cc74874eef286e077e39468977f5b958d
dunp <badpaybad@gmail.com>
2024-04-19 09:38:08 +0700

    test Meta-Llama-3-8B

--- llama3/main.py
+++ llama3/main.py
@@ -3,7 +3,7 @@ import torch
 
 model_id = "/work/Meta-Llama-3-8B"
 pipeline = transformers.pipeline(
-    "text-generation", model=model_id, model_kwargs={"torch_dtype": torch.bfloat16}, device_map="auto"
+    "text-generation", model=model_id, model_kwargs={"torch_dtype": torch.bfloat16}, device_map="cpu"
 )
 
 pipeline("Hey how are you doing today?")
