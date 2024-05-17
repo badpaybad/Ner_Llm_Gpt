01b8e18d420df9aa715e8918e9f91d1b2007da6a
dunp <dunp@omt.vn>
2024-05-17 10:59:26 +0700

    pip3 install -U tensorflow mediapipe mediapipe_model_maker  PyQt5                 pip3 install -U opencv-python opencv-contrib-python

--- mediapipe/train.py
+++ mediapipe/train.py
@@ -7,6 +7,7 @@
 import tensorflow as tf
 import json
 import os
+import mediapipe_model_maker
 from mediapipe_model_maker import object_detector
 import numpy as np
 import matplotlib.rcsetup as rcsetup
