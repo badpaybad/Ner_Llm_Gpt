5b68307352eb6fdbbfc6a3afee7117cfcef4ddba
dunp <badpaybad@gmail.com>
2024-08-03 15:43:53 +0700

    vicuna-7b-v1.5 is better result  https://huggingface.co/lmsys/vicuna-7b-v1.5

--- howtomakeachatbot/textembeding.py
+++ howtomakeachatbot/textembeding.py
@@ -1,29 +1,33 @@
+import time
 from transformers import LlamaTokenizer, LlamaModel, Qwen2Model, Qwen2Tokenizer
 import torch
+import numpy
+from numpy import dot
+from numpy.linalg import norm
 
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
-class TextEmbeding:
-    def __init__(self) :
-        self.modelpath="/work/llm/Ner_Llm_Gpt/mistralvn/Vistral-7B-Chat"
-        pass
-        
 
-    def getTextVector(self,text):
-        print(self.modelpath)
-        tokenizer = AutoTokenizer.from_pretrained( self.modelpath)
-        model = AutoModelForCausalLM.from_pretrained(
-            pretrained_model_name_or_path= self.modelpath,
-            #torch_dtype=torch.bfloat16,  # change to torch.float16 if you're using V100
-            device_map="cpu",# cuda
+class TextEmbeding:
+    def __init__(self):
+        self.modelpath = "/work/shared/vicuna-7b-v1.5"
+        self.tokenizer = AutoTokenizer.from_pretrained(self.modelpath)
+        self. model = AutoModelForCausalLM.from_pretrained(
+            pretrained_model_name_or_path=self.modelpath,
+            # torch_dtype=torch.bfloat16,  # change to torch.float16 if you're using V100
+            device_map="cpu",  # cuda
             use_cache=False,
         )
+        print(self.modelpath)
+        pass
 
-        inputs = tokenizer(text, return_tensors="pt")
+    def getTextVector(self, text):
+
+        inputs = self.tokenizer(text, return_tensors="pt")
 
         # Generate embeddings
         with torch.no_grad():
-            outputs = model(**inputs, output_hidden_states=True)
+            outputs = self.model(**inputs, output_hidden_states=True)
             embeddings = outputs.hidden_states[-1]
 
         embedding = torch.mean(embeddings, dim=1).squeeze().numpy()
@@ -33,6 +37,26 @@ class TextEmbeding:
         return embedding
         pass
 
-test= TextEmbeding()
-res=test.getTextVector("hi du")
-print (res)
+    def consineSimilar(self, a, b):
+
+        cos_sim = numpy.dot(a, b)/(numpy.linalg.norm(a)*numpy.linalg.norm(b))
+        return cos_sim
+
+    def _dot(self, A, B):
+        return (sum(a*b for a, b in zip(A, B)))
+
+    def _cosine_similarity(self, a, b):
+        return self._dot(a, b) / ((self._dot(a, a) ** .5) * (self._dot(b, b) ** .5))
+
+
+test = TextEmbeding()
+res = test.getTextVector("hi du")
+print(len(res))
+
+time.sleep(1)
+
+res1 = test.getTextVector("hi dung")
+print(res1)
+
+print(f"consine similar: {test.consineSimilar(res,res1)}")
+print(f"consine similar1: {test._cosine_similarity(res,res1)}")
